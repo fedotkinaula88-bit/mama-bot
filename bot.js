d86e6d2ec4382dc8770c10e394cc435b71d04046
fedotkinaula88-bit <fedotkinaula88@gmail.com>
2026-05-20 07:21:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
-
+bot.on('message', (msg) => {
+    console.log('Я получил сообщение от: ' + msg.chat.id);
+});
 // Хранилище состояний игроков
 const playersState = {};
 
